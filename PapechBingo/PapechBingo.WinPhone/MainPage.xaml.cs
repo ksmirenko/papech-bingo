c19de73bc70fbb7e7de48966736904a24ac266db
Kirill Smirenko <cyrussmith1865@gmail.com>
2015-10-04 23:29:20 +0300

    Written shared part, drawn WP GUI.

---
+++ PapechBingo/PapechBingo.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace PapechBingo.WinPhone {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page {
+        private ToggleButton[] mainButtons;
+        public MainPage() {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            SetupButtonGrid();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private void SetupButtonGrid() {
+            var gridSize = GridLogic.GRID_SIZE;
+            // calculating button height
+            var scaleFactor = DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
+            var buttonHeight = Window.Current.Bounds.Width * scaleFactor / gridSize;
+            // setting up rows and columns
+            for (var i = 0; i < gridSize; i++) {
+                var colDef = new ColumnDefinition();
+                colDef.Width = new GridLength(1, GridUnitType.Star);
+                mainButtonsGrid.ColumnDefinitions.Add(colDef);
+            }
+            for (var i = 0; i < gridSize; i++) {
+                var rowDef = new RowDefinition();
+                rowDef.Height = new GridLength(1, GridUnitType.Star);
+                mainButtonsGrid.RowDefinitions.Add(rowDef);
+            }
+            // creating and adding buttons
+            mainButtons = new ToggleButton[gridSize * gridSize];
+            for (var i = 0; i < gridSize; i++) {
+                for (var j = 0; j < gridSize; j++) {
+                    var b = new ToggleButton();
+                    b.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    b.VerticalAlignment = VerticalAlignment.Stretch;
+                    b.Height = buttonHeight;
+                    //b.Width = buttonHeight;
+                    b.SetValue(Grid.RowProperty, i);
+                    b.SetValue(Grid.ColumnProperty, j);
+                    b.Padding = new Thickness(0);
+                    b.Margin = new Thickness(0, -11, 0, -11);
+                    // TODO: obtain content from Resources
+                    b.Content = "STUB";
+                    mainButtons[i * gridSize + j] = b;
+                    mainButtonsGrid.Children.Add(b);
+                }
+            }
+        }
+    }
+}
---
+++ PapechBingo/PapechBingo/GridLogic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace PapechBingo {
+    public class GridLogic {
+        public const int GRID_SIZE = 5;
+        public static GridLogic instance = new GridLogic();
+        
+        private bool[,] grid;
+
+        private GridLogic() {
+            grid = new bool[GRID_SIZE, GRID_SIZE];
+            Reset();
+        }
+
+        public Tuple<bool, bool> ToggleButton(int x, int y) {
+            // toggling cell state
+            grid[x, y] = !grid[x, y];
+            var bingo = false;
+            if (grid[x, y]) {
+                // checking bingo
+                var bingoRow = true;
+                for (var j = 0; j < GRID_SIZE; ++j)
+                    bingoRow &= grid[x, j];
+                var bingoColumn = true;
+                for (var i = 0; i < GRID_SIZE; ++i)
+                    bingoColumn &= grid[i, y];
+                var bingoDiag1 = false;
+                if (x == y) {
+                    bingoDiag1 = true;
+                    for (var i = 0; i < GRID_SIZE; ++i)
+                        bingoDiag1 &= grid[i, i];
+                }
+                var bingoDiag2 = false;
+                if (x == GRID_SIZE - y - 1) {
+                    bingoDiag2 = true;
+                    for (var i = 0; i < GRID_SIZE; ++i)
+                        bingoDiag2 &= grid[i, GRID_SIZE - i - 1];
+                }
+                bingo = bingoRow || bingoColumn || bingoDiag1 || bingoDiag2;
+            }
+            return Tuple.Create<bool, bool>(grid[x, y], bingo);
+        }
+
+        public void Reset() {
+            for (var i = 0; i < GRID_SIZE; ++i)
+                for (var j = 0; j < GRID_SIZE; ++j)
+                    grid[i, j] = false;
+        }
+
+        public void FillData(bool[] data) {
+            for (var i = 0; i < GRID_SIZE; ++i)
+                for (var j = 0; j < GRID_SIZE; ++j)
+                    grid[i, j] = data[GRID_SIZE * i + j];
+        }
+
+        public bool[] ExtractData() {
+            var outData = new bool[GRID_SIZE * GRID_SIZE];
+            for (var i = 0; i < GRID_SIZE; ++i)
+                for (var j = 0; j < GRID_SIZE; ++j)
+                    outData[GRID_SIZE * i + j] = grid[i, j];
+            return outData; 
+        }
+    }
+}
+
